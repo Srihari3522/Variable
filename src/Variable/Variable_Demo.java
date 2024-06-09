c614052c7266db4fc9b824d82e2182a93287ad8f
Srihari <sriharireddy033@gmail.com>
2024-06-09 17:28:45 +0530

    First Commit

---
+++ src/Variable/Variable_Demo.java
@@ -0,0 +1,22 @@
+package Variable;
+
+public class Variable_Demo {
+
+	public static void main(String[] args) {
+		int i =0;
+		System.out.println("The value of i is " + i);
+		int j=20;
+		float k,l=24;
+		System.out.println("The value of l is "+ l);
+		k=34;
+		l=30;
+		System.out.println("The value of j is " + j);
+		System.out.println("The value of k is " + k);
+		i++;
+		System.out.println("After increment the value of i is " + i);
+		System.out.println("After increment the value of l is " + l);
+		
+
+	}
+
+}
